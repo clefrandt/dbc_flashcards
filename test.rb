12286f011f9264440bcbf96d8ceb609ffa88ab13
lolcrzykid <mohitr35@gmail.com>
2013-10-18 20:28:56 -0700

    Delete test.rb

--- test.rb
+++
@@ -1,8 +0,0 @@
-mycards = {"Definition1" => "Answer1", "Definition2"=>"Answer2" }
-
-
-# p deck.card_objects 
-# deck.remove_correct_answers
-# puts "Here is the test"
-# p deck.card_objects 
- p deck.make_deck(mycards).length
