ad3e44f57984c1995131035c97d16b0fa078f6a6
Apprentice <apprentice@dbc27.local>
2013-10-18 18:19:38 -0700

    .

--- flashcards.rb
+++ flashcards.rb
@@ -12,7 +12,7 @@
 ##############################
 # Pseudocode Enumeration:
   # Card Parser(C): read definitions and answers into a hash {ans => def, ans2 => def2}
-    # Parses(M): convert txt file to hash
+    # Parse(M): convert txt file to hash
   # Deck(C): flashcard(A), score(A)
     # Storing/holding flashcards(M)
     # Remove flashcard(M)
@@ -24,14 +24,54 @@
     # Display the definition(M): prompt you to guess the answer to a definition 
     # Compare guess and answer(M): compare user input to answer, returns true or false 
     # Display the answer(M): show the answer
-  # User Options(C): Controller code 
+  # User Options(C): View Code 
+    # render
     # Display three options(M):
       # a) Reset(M): Reset the deck of flashcards and start the program over
       # b) Study missed material(M): Start the program over with only the missed cards
       # c) Quit(M): Break and get out of the program
-  # Game(C):
+  # Game(C): Controller Code 
+    # render(ask for file name)
     # Start the game(M): read the text file and parse the text file into an array of game
       # read# game.run!
 
 ##############################
 
+# require 'csv'
+
+class Flashcard_Parser
+  
+  def self.parse(file)
+    place_holder = []
+    File.open(file,'r').each_line do |line|
+      if line != "\n"
+        place_holder << line
+      end
+    end
+
+    parsed_text = Hash[*place_holder]
+    return parsed_text
+  end
+end
+
+
+class Study
+
+  def start!
+    flash_hash = Flashcard_Parser.parse('flashcard_samples.txt')
+    deck = Deck.new(flash_hash)
+    UserInterface.welcome
+    
+  end
+
+end
+
+class UserInterface
+  def welcome
+    puts "get ready to learn!"
+  end
+end
+
+
+current_study_sesh = Study.new
+current_study_sesh.start!
